f16d31cb79101046b788ac0e07a2d87c9757a7a8
baveku <baveku@gmail.com>
2020-04-04 18:39:09 +0700

    Initial Commit

---
+++ TerX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TerX
+//
+//  Created by Bách on 4/4/20.
+//  Copyright © 2020 Bách. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TerX/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TerX
+//
+//  Created by Bách on 4/4/20.
+//  Copyright © 2020 Bách. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
